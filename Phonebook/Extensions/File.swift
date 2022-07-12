b8ba1547c1e1bc6d1087948017a18341c2155877
Kamil Ishmuratov <ishmuratoof@icloud.com>
2022-07-12 20:56:50 +0500

    Remove detail view xib

---
+++ Phonebook/Extensions/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  Phonebook
+//
+//  Created by Камиль on 08.07.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let background = UIColor(named: "Background")
+}
--- Phonebook/Main View/ViewController.swift
+++ Phonebook/Main View/ViewController.swift
@@ -4,109 +4,94 @@
 //
 //  Created by Камиль on 03.10.2021.
 //
-
+ 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController, UITextFieldDelegate {
 
     @IBOutlet weak var searchBar: UISearchBar!
     
-    enum LoadingState {
-        case loading, loaded
-    }
-    
-    var contacts = [Contact]()
-    var searchedContacts = [Contact]()
-    var contactImage = [UIImage]()
-    var imageURL = [String]()
-    var searching = false
-    var contactLoadingState: LoadingState = .loading
-    var imageLoadingState: LoadingState = .loading
+    private var contactsArray = [Contact]()
+    private var searchedContactsArray = [Contact]()
+    private var isSearching = false
+    private var imagesArray = [UIImage?]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Справочник"
-        navigationController?.navigationBar.prefersLargeTitles = true
+        title = Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String
         
         loadData()
+        searchBar.delegate = self
+        searchBar.enablesReturnKeyAutomatically = true
+        searchBar.placeholder = "Search contacts"
+        searchBar.searchTextField.delegate = self
     }
-    
-    // Loading contact data
     
     func loadData() {
-        guard let url = URL(string: "https://randomuser.me/api/?results=1000&inc=name,email,phone,picture") else {
-            print("Invalid URL")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                let decoder = JSONDecoder()
-                if let decodedResponse = try? decoder.decode(Contacts.self, from: data) {
-                    DispatchQueue.main.async {
-                        self.contacts = decodedResponse.results
-                        for result in decodedResponse.results {
-                            self.imageURL.append(result.picture.large)
-                        }
-                        self.tableView.reloadData()
-                    }
-                    self.contactLoadingState = .loaded
-                    return
+        let url = URL(string: "https://randomuser.me/api/?results=5&inc=name,email,phone,picture")!
+
+        DispatchQueue.global().async {
+            let data = try! Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode(Contacts.self, from: data) {
+
+                for result in decoded.results {
+                    let url = URL(string: result.picture.large)!
+                    let data = try! Data(contentsOf: url)
+                    let image = UIImage(data: data)
+                    self.imagesArray.append(image)
+                }
+
+                DispatchQueue.main.async {
+                    self.contactsArray = decoded.results
+                    self.tableView.reloadData()
                 }
             }
-            print("Fetch error")
-        }.resume()
-    }
-    
-    // Loading contact image
-    
-    func loadImage() {        
-        for url in imageURL {
-            let pictureURL = URL(string: url)!
-            
-            let request = URLRequest(url: pictureURL)
-                
-            URLSession.shared.dataTask(with: request) { data, response, error in
-                if let data = data {
-                    DispatchQueue.main.async {
-                        self.contactImage.append(UIImage(data: data)!)
-                    }
-                    
-                    return
-                }
-            }.resume()
         }
     }
-    
-    // Setting a number of rows in tableView
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if searching {
-            return searchedContacts.count
+        if isSearching {
+            return searchedContactsArray.count
         } else {
-            return contacts.count
+            return contactsArray.count
         }
     }
-    
-    // Filling a cell with information
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Contact", for: indexPath)
-        let contact = contacts[indexPath.row]
+        let contact = contactsArray[indexPath.row]
+        let image = imagesArray[indexPath.row]
+
         cell.textLabel?.text = "\(contact.name.first) \(contact.name.last)"
+
+        cell.imageView?.image = image
+        cell.imageView?.layer.cornerRadius = 22
+        cell.imageView?.clipsToBounds = true
+        
         return cell
     }
     
-    // Passing information to detailed view
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let contact = contacts[indexPath.row]
-        let detailedView = DetailedViewController()
-        detailedView.detailedContact = contact
+        let contact = contactsArray[indexPath.row]
+        let detailedView = DetailedViewController(for: contact)
         navigationController?.pushViewController(detailedView, animated: true)
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchedContactsArray = contactsArray.filter({$0.name.first == searchText})
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = true
+        tableView.reloadData()
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
     }
 }
 
